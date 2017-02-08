89e5adc4784e7eebd2b00a61dc2571a4a5360ea4
Martin Kiss <kissko86@gmail.com>
2017-02-08 18:27:12 +0000

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,11 +1,11 @@
 /******************************************************
 SCROLL FUNCTION - USING jQuery Library
-ADD .scrollme class to anchors that link to element 
+ADD .scrollme class to anchors that links to an element 
 
 e.g  <a class="scrollme" href="#myDiv">Scroll</a>
      <div id="myDiv">Scroll To</div>
 
-USEFULL FOR LONG LANDING PAGES -- I GUESS!?
+USEFULL FOR LONG LANDING PAGES -- I GUESS?!?
 *****************************************************/
 $(function() {
   $(".scrollme").on("click", function(e) {
